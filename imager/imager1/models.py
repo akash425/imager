0c5e05cf769cc47b3ea4af7e9c18540db76b6b38
Akash Kushwaha <akashkushwaha209868@gmail.com>
2020-03-29 14:55:15 +0530

    commit2 Models.py modified

--- imager/imager1/models.py
+++ imager/imager1/models.py
@@ -3,3 +3,9 @@ from django.db import models
 # Create your models here.
 class image(models.Model):
     upload = models.ImageField()
+    description = models.CharField(max_lenght=225, blank=True)
+    uploaded_at = models.DateTimeField(auto_now_add=True)
+
+    class meta:
+        verbose_name = 'photo'
+        verbose_name_plural = 'photos'
