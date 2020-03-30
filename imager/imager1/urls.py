29139af62deedb0407b57093c19079be9557d32c
Akash Kushwaha <akashkushwaha209868@gmail.com>
2020-03-30 11:54:24 +0530

    Added two no using Post method

--- imager/imager1/urls.py
+++ imager/imager1/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-    path('', views.home, name='home')
-]
+    path('', views.home, name='base'),
+    path('upload/', views.upload, name='upload'),
+    path('upload/add', views.add, name='add')
+] 
--- imager/imager1/views.py
+++ imager/imager1/views.py
@@ -2,4 +2,15 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'base.html')
+    return render(request, 'base.html', {'uname':'us_name'})
+
+def upload(request):
+    return render(request, 'upload.html')
+
+def add(request):
+
+    val1=int(request.POST['a'])
+    val2=int(request.POST['b'])
+    res=val1+val2
+
+    return render(request, 'result.html', {'result':res})
